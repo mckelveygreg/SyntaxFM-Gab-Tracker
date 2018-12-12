273fc8325ee75da3d924cb6b7a3b0c2cef6b2358
Greg McKelvey <mckelveygreg@gmail.com>
2018-12-12 14:34:12 -0800

    number crunching complete

--- src/TalkingTime.js
+++ src/TalkingTime.js
@@ -1,45 +1,57 @@
-import React, { Component } from 'react'
-import Parser from 'rss-parser' 
+import React, { Component } from "react";
+import Parser from "rss-parser";
 
-const rssURL = 'https://feed.syntax.fm/rss'
+const rssURL = "https://feed.syntax.fm/rss";
 class TalkingTime extends Component {
   state = {
     totalTime: 0,
     sec: 0,
     min: 0,
     hour: 0,
-  }
+    day: 0
+  };
 
   componentDidMount() {
-    let parser = new Parser()
+    let parser = new Parser();
     parser.parseURL(rssURL, (err, feed) => {
-      console.log(feed)
+      console.log(feed);
+      const missingEpidodes = ["02:33", "50:26"]
       let time = feed.items
-        .map(item => item.itunes.duration)
-        .filter(d => d)
-        .map(d => d.split(':') 
-                  .reverse()
-                  .map(d => parseInt(d))
-                  .reduce((prev, curr, i) => prev + curr * Math.pow(60, i)))
-        .reduce((a, b) => a + b)
-        
-        
-        
-        
-        this.setState({totalTime: time})
+        .map(item => item.itunes.duration) // make array of durations
+        .filter(d => d) // filter out undefined
+        .concat(...missingEpidodes)
+        .map(d =>
+          d.split(":") // make array of time values
+            .map(d => parseInt(d)) // convert string to int
+            .reverse() // reverse so reduce works properly
+            .reduce((prev, curr, i) => prev + curr * Math.pow(60, i)) // convert everything to seconds
+        )
+        .reduce((a, b) => a + b); // sum total of seconds
 
+        console.log(time)
+      let secPerDay = 86400; // 60 * 60 * 24
+      let days = Math.floor(time / 60 / 60 / 24);
+      let hours = Math.floor((time % secPerDay) / 60 / 60); // totalTime - days
+      let mins = Math.floor(((time % secPerDay) % (60 * 60)) / 60); // totalTime - days - hours
+      let secs = Math.floor(((time % secPerDay) % (60 * 60)) % 60); // totalTime - days - hours - min
 
-      console.log(time)
-    })
+      this.setState({
+        totalTime: time,
+        day: days,
+        hour: hours,
+        min: mins,
+        sec: secs
+      });
+    });
   }
-
 
   render() {
     return (
       <div>
-        {this.state.totalTime}
+        Total Time: {this.state.totalTime}, {this.state.day}:{this.state.hour}:
+        {this.state.min}:{this.state.sec}
       </div>
-    )
+    );
   }
 }
 
